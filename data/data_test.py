f7945d3f96abdd1f0f4c9d53551755dffe76af2e
bolayangnum1 <bolot.muxa@gmail.com>
2022-12-12 15:59:26 +0600

    start

--- data/data_test.py
+++ data/data_test.py
@@ -15,25 +15,16 @@ def url_address():
 
 def file_write(url, file_name):
     jsondata = requests.get(url).json()[0]
-    # result = jsondata['display_name'], jsondata['lat'], jsondata['lon'], jsondata['place_id']
     result = jsondata['display_name'], jsondata['lat'], jsondata['lon'], jsondata['place_id']
     with open(file_name, 'w') as outfile:
         json.dump(result, outfile)
-    # data_file = open(file_name, 'w', newline='')
-    # csv_writer = csv.writer(data_file)
-    # for data in jsondata:
-    #     csv_writer.writerow(data.values())
-    # data_file.close()
-    # return data_file
 
 
 def open_file_coordinates_and_address(file, url, file_name):
     if os.path.exists(file) == True:
-        return csv.reader(file)
+        with open(file) as f:
+            return json.load(f)
     else:
         file_write(url, file_name)
-        return csv.reader(file)
-
-
-# g = open_file_coordinates_and_address('data/address.json', url_address(), 'data/address.json')
-# print(g)
+        with open(file) as f:
+            return json.load(f)
